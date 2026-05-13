5c59d76b460cf6789f0dc16419fa19b303c76cb6
Tohibur <tohibur.tauhid@gmail.com>
2026-05-13 07:13:29 +0530

    Fix: AuthProvider path, helmet provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import router from "./routes/Routes";
 import { RouterProvider } from "react-router";
 import AuthProvider from "./provider/AuthProvider";
+import { HelmetProvider } from "react-helmet-async";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
   <AuthProvider>
+    <HelmetProvider>
       <RouterProvider router={router} />
+    </HelmetProvider>
   </AuthProvider>
 );
